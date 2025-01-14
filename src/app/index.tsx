01ccfc58befbc4c471ce8236bc99a14b4e3410b6
Philip Double <admin@cognizen.com>
2025-01-14 13:43:38 -0500

    added public folder and config.json - updated webpack to copy public folder to dist on build so that config.json can dynamically load at runtime

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,34 @@
-import * as React from 'react';
-import { RouterProvider } from 'react-router-dom';
-import { router } from '@app/routes';
 import '@app/app.css';
 import '@patternfly/react-core/dist/styles/base.css';
 import '@patternfly/chatbot/dist/css/main.css';
 
-const App: React.FunctionComponent = () => <RouterProvider router={router} />;
+import * as React from 'react';
+
+import { RouterProvider } from 'react-router-dom';
+import { router } from '@app/routes';
+
+const App: React.FunctionComponent = () => {
+  const [config, setConfig] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('/public/config.json')
+      .then((response) => {
+        console.log(response);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => setConfig(data))
+      .catch((error) => console.error('Error loading config:', error));
+  }, []);
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
 
 export default App;
